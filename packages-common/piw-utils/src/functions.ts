57cced3beb482d434f631d706359f8a7c2a3ad57
Mehmet Ali IZCI <mehmet.izci@mendix.com>
2019-11-28 11:10:22 +0100

    feat(piw-utils): add new function

--- packages-common/piw-utils/src/functions.ts
+++ packages-common/piw-utils/src/functions.ts
@@ -1,7 +1,13 @@
-import { ActionValue } from "mendix";
+import { ActionValue, EditableValue, ValueStatus } from "mendix";
 
 export const executeAction = (action?: ActionValue): void => {
     if (action && action.canExecute && !action.isExecuting) {
         action.execute();
     }
 };
+
+export function setAttributeValue<T extends any>(attribute?: EditableValue<T>, value?: T): void {
+    if (attribute && !attribute.readOnly && attribute.status === ValueStatus.Available) {
+        attribute.setValue(value);
+    }
+}
